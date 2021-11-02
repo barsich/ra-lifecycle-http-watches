492ab012f4bb355aac1769129a1742b93d78d843
barsich <barsicchan@gmail.com>
2021-11-02 18:14:09 +0500

    some fixes

--- src/components/Watches/WatchesList/WatchItem/index.js
+++ src/components/Watches/WatchesList/WatchItem/index.js
@@ -4,17 +4,14 @@ import { calcTime } from '../../../../utils';
 export default class WatchesItem extends Component {
   constructor(props) {
     super(props);
-    this.item = props.item;
-    this.name = this.item.name;
-    this.timezone = this.item.timezone;
     this.state = {
-      time: calcTime(this.item.timezone),
+      time: calcTime(this.props.item.timezone),
     };
     this.timer = null;
   }
 
   componentDidMount() {
-    this.timer = setInterval(() => this.setState({ time: calcTime(this.item.timezone) }), 1000);
+    this.timer = setInterval(() => this.setState({ time: calcTime(this.props.item.timezone) }), 1000);
   }
 
   componentWillUnmount() {
@@ -25,12 +22,12 @@ export default class WatchesItem extends Component {
     return (
       <div className="watches-list__item">
         <div className="watches-list__item-body">
-          <p className="watches-list__item-name">{this.item.name}</p>
+          <p className="watches-list__item-name">{this.props.item.name}</p>
           <p className="watches-list__item-time">{this.state.time}</p>
         </div>
         <div
           className="watches-list__item-delete material-icons"
-          onClick={() => this.props.onDelete(this.item.id)}
+          onClick={() => this.props.onDelete(this.props.item.id)}
         >
           close
         </div>
